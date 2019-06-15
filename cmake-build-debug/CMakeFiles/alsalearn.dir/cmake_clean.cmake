f4fa2928519ed9aa470ca8c2cfe7d89703d44625
wangchao <watcher.hangzhou@foxmail.com>
2019-06-15 09:58:46 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/alsalearn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/alsalearn.dir/main.c.o"
+  "alsalearn.pdb"
+  "alsalearn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/alsalearn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
